914f0e62f5871ae0b6daf7da20e8e4428d4ddf69
Adriano Rocha <adriano.olr@gmail.com>
2017-02-11 18:31:48 -0300

    Script Raycast

---
+++ Assets/Scripts/InfoPopUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPopUp : MonoBehaviour {
+
+	Canvas popUp;
+
+	public void showPopUp () {
+		popUp.enabled = true;
+	}
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	int h, w;
+
+	void Start () {
+		h = Screen.height / 2;
+		w = Screen.width / 2;
+	}
+
+	void Update () {
+		if (Input.touchCount > 0 || Input.GetButton("Fire1")) {
+			Ray ray = Camera.main.ScreenPointToRay (new Vector3 (h, w, 0));
+
+			Debug.DrawRay (ray.origin, ray.direction * 1000, Color.red);
+		}
+	}
+}
